e214937288743d3860f46c20eee41707a621155c
Alberto Navalon <alberto.navalonlillo@gmail.com>
2020-01-03 23:43:59 +0100

    Macros header file (to save space in main)

---
+++ src/macros.hpp
@@ -0,0 +1,25 @@
+#ifndef RUBIK_MACROS_HPP
+#define RUBIK_MACROS_HPP
+
+// colors
+#define WHITE 1
+#define YELLOW 2
+#define GREEN 3
+#define RED 4
+#define BLUE 5
+#define ORANGE 6
+
+// input method (IO)
+#define FINPUT 0
+#define LINPUT 1
+
+// error codes
+#define ECRASH 0
+#define EWRONGINPUT 1
+#define EWRONGCOLORINPUT 2
+#define ENOCOLOR 3
+#define EINDEXOUTOFRANGE 4
+// misc
+#define two for(int i=0;i<2;i++)
+
+#endif
